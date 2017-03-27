1962e4b67af681b82079a546e2448cdb7929d155
guanyelong <517477668@qq.com>
2017-03-27 14:14:59 +0800

    master

---
+++ BBD.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BBD.Web.Startup))]
+namespace BBD.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
