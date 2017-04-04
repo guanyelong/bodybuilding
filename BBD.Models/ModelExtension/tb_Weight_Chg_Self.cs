5c3739313872b6807f508612545eb93876eefbe2
guanyelong <517477668@qq.com>
2017-04-04 23:33:36 +0800

    888

---
+++ BBD.Models/ModelExtension/tb_Weight_Chg_Self.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBD.Models
+{
+    public partial class tb_Weight_Chg_Self
+    {
+        public string uName { get; set; }
+        public string Mobile { get; set; }
+    }
+}
